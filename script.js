05b3fe3425b55febd71e39d0997862f642d5ba25
Lal Gurung <ljgpok@yahoo.com>
2020-06-06 11:42:47 +0300

    intro-component-with signup-form-master added

---
+++ script.js
@@ -0,0 +1,42 @@
+const projects = [
+	{
+		name: 'four-card-feature-section-master',
+		github:
+			'https://github.com/t1gula00/Frontend-Mentor/tree/master/four-card-feature-section-master'
+	},
+	{
+		name: 'intro-component-with-signup-form-master',
+		github:
+			'https://github.com/t1gula00/Frontend-Mentor/tree/master/intro-component-with-signup-form-master'
+	}
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name, github }, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="/${name}/index.html">
+			<img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+		<div class="links-container">
+			<a href="/${name}/index.html" class="blue">
+				<i class="fas fa-eye"></i>
+			</a>
+			<a href="${github}" class="youtube">
+				<i class="fab fa-github"></i>
+			</a>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
